25b76758398790d02c8b4cea460bd1d70438471b
Jake Sikora <jakesikora.01@gmail.com>
2013-11-07 14:29:13 -0600

    pivoted: no longer using draggable

--- application.js
+++ application.js
@@ -1,49 +1,35 @@
 $(function() {
-  $('.movable').each(function(index,element) {
-    this.setAttribute('identifier', index)
-    
-    var attrs = {}
-    attrs.left = $(element).offset().left;
-    attrs.top = $(element).offset().top;
-    attrs.right = attrs.left + width
-    attrs.bottom = attrs.top + height
-    attrs.index = index
+  createBoxes();
 
-    totalObjects.push(new movableBox(attrs));
-  })
-
-  $('.movable').draggable({ containment: "parent",
-                            grid: [ 100, 100 ],
-                            drag: function(e, ui) {
-                              var index = $(this).attr('identifier');
-                              // if the box moves update its position
-                              // need to figure make this logic only work when it actually moves
-                              // not each time you drag it any amount
-                              if(ui.position.left===100){
-                                totalObjects[index].updatePositionHorizontal(ui.position.left)
-                              }
-                            }
-  });
 
 });
 
 var totalObjects = [];
 
-var width = 98
-var height = 98
+function createBoxes() {
+  $('.movable').each(function(index,element) {
+    this.setAttribute('identifier', index);
 
-function movableBox(attrs) {
-  this.identifier = attrs.index
-  this.left = attrs.left
-  this.top = attrs.top
-  this.right = attrs.right
-  this.bottom = attrs.bottom
+    var attrs = {};
+    attrs.left = $(element).offset().left;
+    attrs.top = $(element).offset().top;
+
+    attrs.index = index;
+
+    totalObjects.push(new movableBox(attrs));
+  })
 }
 
-movableBox.prototype.updatePositionHorizontal = function(left) {
-  this.left += left
-  // this.top = position.top
-  // this.right = this.left + width
-  // this.bottom = this.top + height
-  console.log(this);
+function movableBox(attrs) {
+  this.identifier = attrs.index;
+  this.left = attrs.left;
+  this.top = attrs.top;
+
+  this.newLeft = this.left;
+  this.newTop = this.top;
+}
+
+movableBox.prototype.updatePosition = function(left, top) {
+  this.newLeft = left + this.left;
+  this.newTop = top + this.top;
 }
