1cc29809162822a8881aab598f8ba0558a7841af
Jake Sikora <jakesikora.01@gmail.com>
2013-11-07 15:25:13 -0600

    adding correct directional move class to tiles

--- application.js
+++ application.js
@@ -1,35 +1,43 @@
 $(function() {
-  createBoxes();
-
-
+  establishBoard();
 });
 
-var totalObjects = [];
+var width = 100
+var height = 100
 
-function createBoxes() {
-  $('.movable').each(function(index,element) {
-    this.setAttribute('identifier', index);
-
-    var attrs = {};
-    attrs.left = $(element).offset().left;
-    attrs.top = $(element).offset().top;
-
-    attrs.index = index;
-
-    totalObjects.push(new movableBox(attrs));
+function establishBoard() {
+  $('.outer-box').children().each(function(index, element) {
+    this.setAttribute('pos-left', $(element).offset().left);
+    this.setAttribute('pos-top', $(element).offset().top);
   })
+  declareMovable();
 }
 
-function movableBox(attrs) {
-  this.identifier = attrs.index;
-  this.left = attrs.left;
-  this.top = attrs.top;
+function declareMovable() {
+  $('.tile').each(function(index, tile) {
+    var tileIsLeft  = parseInt($(tile).attr('pos-left')) + 100  == $('.empty').attr('pos-left')
+    var tileIsRight = parseInt($(tile).attr('pos-left')) - 100 == $('.empty').attr('pos-left')
+    var tileIsAbove = parseInt($(tile).attr('pos-top'))  + 100 == $('.empty').attr('pos-top')
+    var tileIsBelow = parseInt($(tile).attr('pos-top'))  - 100 == $('.empty').attr('pos-top')
 
-  this.newLeft = this.left;
-  this.newTop = this.top;
-}
+    var tileIsHorizontal = $(tile).attr('pos-top')  == $('.empty').attr('pos-top')
+    var tileIsVertical   = $(tile).attr('pos-left') == $('.empty').attr('pos-left')
 
-movableBox.prototype.updatePosition = function(left, top) {
-  this.newLeft = left + this.left;
-  this.newTop = top + this.top;
+    // declare class right movable
+    if(tileIsLeft && tileIsHorizontal) {
+      $(this).addClass('move-right');
+    }
+    // declare class left movable
+    else if(tileIsRight && tileIsHorizontal) {
+      $(this).addClass('move-left');
+    }
+    // decalre class up movable
+    else if(tileIsBelow && tileIsVertical) {
+      $(this).addClass('move-up');
+    }
+    // declare class down movable
+    else if(tileIsAbove && tileIsVertical) {
+      $(this).addClass('move-down');
+    }
+  });
 }
