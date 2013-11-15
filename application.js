ad2b4654ba5996ca791b488a7c5195d7c5de4a78
Jake Sikora <jakesikora.01@gmail.com>
2013-11-15 14:10:29 -0600

    single-toggle functionality working

--- application.js
+++ application.js
@@ -9,34 +9,57 @@ board = {
  
   startGame : function($el) {
     this.$tiles = $el
+    this.addIndex();
     this.size = this.$tiles.length;
     this.generate();
-    this.render();
+    this.clickMe();
+  },
+
+  clickMe : function() {
+    $('.tile').on('click', function() {
+      var index = $(this).data('index');
+      board.tilesArray[Math.floor(index/5)][index%5].toggleStatus();
+      board.render();
+    })
   },
  
+  addIndex : function() {
+    this.$tiles.each(function(index) {
+      $(this).data('index',index);
+    });
+  },
+
   generate : function() {
     var row = [];
     this.$tiles.each(function() {
       if(row.length < 5) {
-        row.push(new Tile);
+        row.push(new Tile($(this)));
       } else {
         board.tilesArray.push(row);
         row = [];
-        row.push(new Tile);
+        row.push(new Tile($(this)));
       }
     });
     this.tilesArray.push(row);
+    this.render();
   },
 
+  // Refactor this to generate map once
+  // new function would act recursively to render
+  // tiles that are clicked, and the ones touching it
   render : function() {
-    for(var i=0;i<this.size;i++) {
-      $('#outer-box :nth-child('+i+')').addClass(this.tilesArray[Math.floor(i/5)][i%5].status);
-    }
+    $(this.tilesArray).each(function() {
+      $(this).each(function() {
+        $(this.domElement).removeClass();
+        $(this.domElement).addClass("tile "+this.status); 
+      })
+    })
   }
 }
 
 // ______________ Tiles Stuff _____________
-function Tile() {
+function Tile($el) {
+  this.domElement = $el
   this.status = this.generateStatus();
 }
 
